9c8ded99877089842e6e668355528b4442779c5e
Darren White <darrenswhite95@gmail.com>
2021-04-11 12:12:40 +0100

    Add specie base stat total
    Fix #1

--- client/src/modules/species-info/SpecieStats.tsx
+++ client/src/modules/species-info/SpecieStats.tsx
@@ -5,6 +5,7 @@ import React, {useContext} from 'react';
 import {GenerationContext} from '../generation/GenerationProvider';
 
 const MAX_STAT = 255;
+const MAX_TOTAL = MAX_STAT * 6;
 const STAT_COLORS: StatsTable<Record<number, string>> = {
   hp: colors.red,
   atk: colors.amber,
@@ -13,6 +14,7 @@ const STAT_COLORS: StatsTable<Record<number, string>> = {
   spd: colors.green,
   spe: colors.purple,
 };
+const STAT_TOTAL_COLOR = colors.brown;
 
 const useStyles = makeStyles(theme => ({
   stat: {
@@ -41,6 +43,47 @@ export const SpecieStats: React.FC<SpecieStatsProps> = ({
 }: SpecieStatsProps) => {
   const {generation} = useContext(GenerationContext);
   const classes = useStyles();
+  const baseStatTotal = Object.values(specie.baseStats).reduce(
+    (left, right) => left + right,
+    0
+  );
+
+  const renderStatName = (name: string) => {
+    return (
+      <div className={`${classes.stat} ${classes.statName}`}>
+        <Typography variant="caption">{name}</Typography>
+      </div>
+    );
+  };
+
+  const renderStatBar = (
+    value: number,
+    max: number,
+    color: Record<number, string>
+  ) => {
+    const width = (value / max) * 100;
+
+    return (
+      <Grid container alignItems="center" wrap="nowrap">
+        <Grid
+          item
+          className={`${classes.stat} ${classes.statBarContainer}`}
+          style={{backgroundColor: color[300]}}
+        >
+          <div
+            style={{
+              backgroundColor: color[700],
+              width: `${width}%`,
+            }}
+          />
+        </Grid>
+
+        <Grid item className={classes.statValue}>
+          <Typography variant="caption">{value}</Typography>
+        </Grid>
+      </Grid>
+    );
+  };
 
   return (
     <>
@@ -52,45 +95,25 @@ export const SpecieStats: React.FC<SpecieStatsProps> = ({
 
       <Grid container justify="center" spacing={1}>
         <Grid item>
-          {Object.keys(specie.baseStats).map(stat => (
-            <div key={stat} className={`${classes.stat} ${classes.statName}`}>
-              <Typography variant="caption">
-                {generation
-                  ? generation.stats.display(stat as StatName)
-                  : stat.toUpperCase()}
-              </Typography>
-            </div>
-          ))}
+          {Object.keys(specie.baseStats).map(stat => {
+            return renderStatName(
+              generation ? generation.stats.display(stat) : stat.toUpperCase()
+            );
+          })}
+
+          {renderStatName('Total')}
         </Grid>
 
         <Grid item xs>
           {Object.entries(specie.baseStats).map(([stat, value]) => {
-            const width = (value / MAX_STAT) * 100;
-            const color = STAT_COLORS[stat as StatName];
-            const backgroundColor = color[700];
-            const backgroundColorLighter = color[300];
-
-            return (
-              <Grid key={stat} container alignItems="center" wrap="nowrap">
-                <Grid
-                  item
-                  className={`${classes.stat} ${classes.statBarContainer}`}
-                  style={{backgroundColor: backgroundColorLighter}}
-                >
-                  <div
-                    style={{
-                      backgroundColor,
-                      width: `${width}%`,
-                    }}
-                  />
-                </Grid>
-
-                <Grid item className={classes.statValue}>
-                  <Typography variant="caption">{value}</Typography>
-                </Grid>
-              </Grid>
+            return renderStatBar(
+              value,
+              MAX_STAT,
+              STAT_COLORS[stat as StatName]
             );
           })}
+
+          {renderStatBar(baseStatTotal, MAX_TOTAL, STAT_TOTAL_COLOR)}
         </Grid>
       </Grid>
     </>
